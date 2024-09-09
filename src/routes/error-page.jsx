96f9377e107f263ac00d9dd04e63e2fe4d0f2c27
Andrey Semenov <semenov.orc@gmail.com>
2024-09-09 18:52:54 +0300

    add nav bar to error-page

--- src/routes/error-page.jsx
+++ src/routes/error-page.jsx
@@ -1,19 +1,23 @@
 import { useRouteError } from 'react-router-dom'
+import Menu from '../components/Menu/Menu'
 
 export default function ErrorPage() {
   const error = useRouteError()
   console.error(error)
 
   return (
-    <div
-      id='error-page'
-      className='page error'
-    >
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <>
+      <Menu />
+      <div
+        id='error-page'
+        className='page error'
+      >
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
+    </>
   )
 }
